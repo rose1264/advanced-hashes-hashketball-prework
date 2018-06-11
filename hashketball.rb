acbc16cf7f3588378c60458aa5b06e0d188a3caf
Shumei Huang <rose1264@gmail.com>
2018-06-11 15:12:50 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,176 @@
 # Write your code here!
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: [
+        {
+          player_name: "Alan Anderson",
+          number: 0,
+          shoe: 16,
+          points: 22,
+          rebounds: 12,
+          assists: 12,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 1
+        }, {
+          player_name: "Reggie Evans",
+          number: 30,
+          shoe: 14,
+          points: 12,
+          rebounds: 12,
+          assists: 12,
+          steals: 12,
+          blocks: 12,
+          slam_dunks: 7
+        }, {
+          player_name: "Brook Lopez",
+          number: 11,
+          shoe: 17,
+          points: 17,
+          rebounds: 19,
+          assists: 10,
+          steals: 3,
+          blocks: 1,
+          slam_dunks: 15
+        }, {
+          player_name: "Mason Plumlee",
+          number: 1,
+          shoe: 19,
+          points: 26,
+          rebounds: 12,
+          assists: 6,
+          steals: 3,
+          blocks: 8,
+          slam_dunks: 5
+        }, {
+          player_name: "Jason Terry",
+          number: 31,
+          shoe: 15,
+          points: 19,
+          rebounds: 2,
+          assists: 2,
+          steals: 4,
+          blocks: 11,
+          slam_dunks: 1
+        }
+      ]
+    },
+    away: {
+      team_name: "Charlotte Hornets",
+      colors: ["Turquoise", "Purple"],
+      players: [
+        {
+          player_name: "Jeff Adrien",
+          number: 4,
+          shoe: 18,
+          points: 10,
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
+        }, {
+          player_name: "Bismak Biyombo",
+          number: 0,
+          shoe: 16,
+          points: 12,
+          rebounds: 4,
+          assists: 7,
+          steals: 7,
+          blocks: 15,
+          slam_dunks: 10
+        }, {
+          player_name: "DeSagna Diop",
+          number: 2,
+          shoe: 14,
+          points: 24,
+          rebounds: 12,
+          assists: 12,
+          steals: 4,
+          blocks: 5,
+          slam_dunks: 5
+        }, {
+          player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0
+        }, {
+          player_name: "Brendan Haywood",
+          number: 33,
+          shoe: 15,
+          points: 6,
+          rebounds: 12,
+          assists: 12,
+          steals: 22,
+          blocks: 5,
+          slam_dunks: 12
+        }
+      ]
+    }
+  }
+end
 
+# helper method
+# return a hash collection of all players
+def players
+  home_players = game_hash.fetch(:home).fetch(:players)
+  away_players = game_hash.fetch(:away).fetch(:players)
+  home_players + away_players
+end
+# find can also work on hash, return the found hash
+# once a method is defined, it can be used in another method?
+def find_player(name)
+  players.find {|player| player.fetch(:player_name) == name}
+end
 
+def num_points_scored(name)
+  find_player(name)[:points]
+end
 
+def shoe_size(name)
+  find_player(name)[:shoe]
+end
 
+def teams
+  game_hash.values
+end
 
+def find_team(team_name)
+  teams.find{|team| team.fetch(:team_name) == team_name}
+end
 
+def team_colors(team)
+  find_team(team)[:colors]
+end
 
+def team_names
+  teams.map do |team|
+    team[:team_name]
+  end
+end
 
+def player_numbers(team_name)
+  find_team(team_name)[:players].map do |player|
+    player[:number]
+  end
+end
 
+def player_stats(name)
+  find_player(name).reject {|key, value| key == :player_name}
+end
 
+def big_shoe_size
+  players.sort_by {|player| player.fetch(:shoe)}.last
+end
+
+def big_shoe_rebounds
+  big_shoe_size.fetch(:rebounds)
+end
